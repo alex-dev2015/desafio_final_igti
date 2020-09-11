1d9bd004fd52d5555a1e2e915200a603658ab417
AlexSousa <alex.sousa20@hotmail.com>
2020-09-11 13:03:26 -0300

    First commit heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const api = axios.create({ baseURL: 'api' })
 
 export default function App() {
-  return <h1>Desafio Final do Bootcamp Full Stack</h1>;
+
+  const [ transactions, setTransactions ] = useState([])
+
+  useEffect(() => {
+    const fetchTransactions = async () =>{
+      const { data } = await api.get('/transaction?period=2019-07');
+      
+      setTransactions(data.transactions);
+    }
+
+    fetchTransactions();
+  }, [])
+
+  return (
+      <div className='container'>
+        <h2 className='center'>Desafio Final do Bootcamp Full Stack</h2>
+
+        {
+          transactions.map(item => {
+            return <p key={item}>{item}</p>
+          })
+        }
+
+      </div>
+    
+    );
 }

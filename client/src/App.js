3f1de7e7394acf1f3bfa3ef93b2682353c44340e
AlexSousa <alex.sousa20@hotmail.com>
2020-09-11 17:47:44 -0300

    Criado o esqueleto das requisições

--- client/src/App.js
+++ client/src/App.js
@@ -27,6 +27,10 @@ export default function App() {
           })
         }
 
+        
+
+
+
       </div>
     
     );
--- routes/routes.js
+++ routes/routes.js
@@ -17,6 +17,72 @@ transactionRouter.get('/', async(request, response) => {
         console.log(message);
         response.status(400).send({error:message});
     }
-})
+});
+transactionRouter.post('/', async(request, response) => {
+    const { body } = request;
+
+    try {
+        if (JSON.stringify(body) === '{}') {
+            throw new Error(`Conteúdo inexistente`)
+        }
+        response.send({status: 'Ok'});
+    } catch ({message}) {
+        console.log(message);
+        response.status(400).send({error:message});
+    }
+});
+
+transactionRouter.put('/', async(request, response) => {
+    
+    try {
+
+        throw new Error(`Id inexistente`)
+        
+    } catch ({message}) {
+        console.log(message);
+        response.status(400).send({error:message});
+    }
+});
+
+transactionRouter.put('/:id', async(request, response) => {
+    const { body, params } = request;
+
+    try {
+
+            if (JSON.stringify(body) === '{}') {
+            throw new Error(`Conteúdo inexistente`)
+        }
+        response.send({status: 'Ok'});
+    } catch ({message}) {
+        console.log(message);
+        response.status(400).send({error:message});
+    }
+});
+
+transactionRouter.delete('/', async(request, response) => {
+    
+    try {
+
+        throw new Error(`Id inexistente`)
+        
+    } catch ({message}) {
+        console.log(message);
+        response.status(400).send({error:message});
+    }
+});
+
+transactionRouter.delete('/:id', async(request, response) => {
+    const { params } = request;
+
+    try {
+
+        response.send({status: 'Ok'});
+    } catch ({message}) {
+        console.log(message);
+        response.status(400).send({error:message});
+    }
+});
+
+
 
 module.exports = transactionRouter;
